4158f0a806b5e30f1b1a097ac1c98c84b2b68a55
Shent <shentov@gmail.com>
2017-05-08 16:36:33 +0100

    EJS Example

--- server.js
+++ server.js
@@ -1,3 +1,15 @@
-/**
- * Created by shent on 08/05/2017.
- */
+import express from 'express';
+
+const app = express();
+
+app.use(express.static(__dirname + '/public'));
+
+app.set('view engine', 'ejs');
+
+app.get('/', (request, response) => {
+    response.render('index',{message:'Hello EJS'});
+});
+
+app.listen(3000, () => {
+    console.log('Express app listening on port 3000');
+});
